9d8340ad365a710323604d30575cec031366ba4a
Med Aziz YAHYAOUI <46204502+azizyahyaoui@users.noreply.github.com>
2021-11-04 16:47:40 +0100

    upload files

---
+++ Challenge2_UnityTuto/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+  
+    private float startDelay = 2.0f;
+    private float spawnInterval=30.0f;
+    float health = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("SpownDogs", startDelay, spawnInterval);
+         
+        }
+    }
+
+    void SpownDogs()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+  
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # unity-tutorials
+My profile: https://learn.unity.com/u/6130a30aedbc2a0644057c2f?tab=activity
